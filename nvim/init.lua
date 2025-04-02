eb3695ab4a96396b0ea78126ce6ba9b1c874ba2f
ksp <peter.kaiser@heller.biz>
2025-04-02 16:54:11 +0200

    fix: windows config

---
+++ nvim/init.lua
@@ -0,0 +1,62 @@
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins')
+require('lualine').setup()
+require('neoconf').setup()
+
+vim.cmd[[colorscheme ksp]]
+
+vim.api.nvim_command([[autocmd VimEnter * lua vim.api.nvim_command('silent !echo -ne "\\e[2 q"')]])
+
+vim.opt.termguicolors = true
+vim.opt.guifont = "Iosevka NF:h12"
+vim.opt.guicursor = "a:block-blinkon0"
+-- vim.o.t_SI = ""
+-- vim.o.t_EI = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,26 @@
+return {
+  "github/copilot.vim",
+  "navarasu/onedark.nvim",
+  -- "folke/tokyonight.nvim",
+  -- "folke/neodev.nvim",
+  "editorconfig/editorconfig-vim",
+  -- "tpope/vim-fugitive",
+  {"folke/which-key.nvim", event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "nvim-lualine/lualine.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "nvim-treesitter/nvim-treesitter",
+  "neovim/nvim-lspconfig",
+  -- "nvim-lua/plenary.nvim",
+  "nvim-telescope/telescope.nvim",
+}
